7ae21d0a9b3aa6938e5d5949611dbd43c0923310
Felix Boström <46707048+bostrum@users.noreply.github.com>
2025-03-12 21:09:48 +0100

    new embedded version

---
+++ systemtappd-embedded/embedded.js
@@ -0,0 +1,86 @@
+function extractDynamicPart(htmlContent) {
+    var match = htmlContent.match(/_next\/static\/([^\/]+)\/_buildManifest\.js/);
+    return match ? match[1] : null;
+}
+
+function getBeer() {
+    console.log("SYSTEMTAPPD: Button Clicked");
+    const currentURL = window.location.href;
+    fetch(currentURL)
+        .then(response => response.text())
+        .then(htmlContent => {
+            var dynamicPart = extractDynamicPart(htmlContent);
+            var productNameMatch = currentURL.match(/\/([^\/]+)\/$/);
+            var productName = productNameMatch ? productNameMatch[1] : null;
+
+            if (dynamicPart && productName) {
+                var jsonURL = 'https://www.systembolaget.se/_next/data/' + dynamicPart + '/produkt/ol/' + productName + '.json';
+                fetch(jsonURL)
+                    .then(response => response.json())
+                    .then(data => {
+                        var title = data.pageProps.seo.openGraph.title.toLowerCase().replace(/\s+/g, '+');
+                        console.log('SYSTEMTAPPD: Logging:', title);
+
+                        var untappdURL = 'https://untappd.com/search?q=' + title;
+                        window.open(untappdURL, '_blank');
+                    })
+                    .catch(error => {
+                        alert("Could not fetch JSON from Systembolaget with link: " + jsonURL);
+                        console.error('SYSTEMTAPPD: Error fetching JSON:', error);
+                    });
+            } else {
+                alert("Dynamic part extraction failed or product name not found.");
+                console.error('SYSTEMTAPPD: Failed to extract dynamic part or match product name.');
+            }
+        })
+        .catch(error => {
+            alert("Could not fetch HTML content from the current URL.");
+            console.error('SYSTEMTAPPD: Error fetching HTML content:', error);
+        });
+}
+
+function addButton() {
+    const targetElement = document.querySelector('.css-9egk76.enbz1310');
+    console.log('SYSTEMTAPPD: Target element:', targetElement);
+
+    if (targetElement) {
+        const brElement = document.createElement('br');
+        const newButton = document.createElement('button');
+        newButton.textContent = 'Untappd';
+        newButton.className = 'css-9egk76 enbz1310';
+        newButton.style.backgroundColor = "#ffc000";
+        newButton.style.color = "white";
+
+        newButton.addEventListener("mouseover", function() {
+            newButton.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+        });
+
+        newButton.addEventListener("mouseout", function() {
+            newButton.style.boxShadow = "none";
+        });
+
+        newButton.addEventListener('click', () => {
+            console.log("SYSTEMTAPPD: Button clicked");
+            getBeer();
+        });
+
+        targetElement.parentNode.insertBefore(brElement, targetElement.nextSibling);
+        targetElement.parentNode.insertBefore(newButton, brElement.nextSibling);
+        console.log('SYSTEMTAPPD: New button added');
+    } else {
+        console.log('SYSTEMTAPPD: Target element not found');
+    }
+}
+
+const observer = new MutationObserver((mutations, obs) => {
+    const targetElement = document.querySelector('.css-9egk76.enbz1310');
+    if (targetElement) {
+        addButton();
+        obs.disconnect();
+    }
+});
+
+observer.observe(document, {
+    childList: true,
+    subtree: true
+});
---
+++ systemtappd-popup/popup.js
@@ -0,0 +1,55 @@
+console.log("Logging: Start");
+
+function getCurrentTabUrl(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var currentTab = tabs[0];
+        var currentTabURL = currentTab.url;
+        callback(currentTabURL);
+    });
+}
+
+function extractDynamicPart(htmlContent) {
+    var match = htmlContent.match(/_next\/static\/([^\/]+)\/_buildManifest\.js/);
+    return match ? match[1] : null;
+}
+
+function getBeer() {
+    console.log("Logging: Button Clicked");
+    getCurrentTabUrl(function(currentURL) {
+        fetch(currentURL)
+            .then(response => response.text())
+            .then(htmlContent => {
+                var dynamicPart = extractDynamicPart(htmlContent);
+                var productNameMatch = currentURL.match(/\/([^\/]+)\/$/);
+                var productName = productNameMatch ? productNameMatch[1] : null;
+
+                if (dynamicPart && productName) {
+                    var jsonURL = 'https://www.systembolaget.se/_next/data/' + dynamicPart + '/produkt/ol/' + productName + '.json';
+                    fetch(jsonURL)
+                        .then(response => response.json())
+                        .then(data => {
+                            var title = data.pageProps.seo.openGraph.title.toLowerCase().replace(/\s+/g, '+');
+                            console.log('Logging:', title);
+
+                            var untappdURL = 'https://untappd.com/search?q=' + title;
+                            window.open(untappdURL, '_blank');
+                        })
+                        .catch(error => {
+                            alert("Could not fetch JSON from Systembolaget with link: " + jsonURL);
+                            console.error('Logging: Error fetching JSON:', error);
+                        });
+                } else {
+                    alert("Dynamic part extraction failed or product name not found.");
+                    console.error('Logging: Failed to extract dynamic part or match product name.');
+                }
+            })
+            .catch(error => {
+                alert("Could not fetch HTML content from the current URL.");
+                console.error('Logging: Error fetching HTML content:', error);
+            });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("searchUntappd").addEventListener("click", getBeer);
+});
